e4f945cc1424247a2594499d5fb163546be7824f
David <davidpollockcsu@gmail.com>
2025-07-08 18:14:05 -0700

    pipeline failing.

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=(".env"))
     SMTP_HOST: str
     SMTP_PORT: str
     SMTP_USERNAME: EmailStr
@@ -12,6 +11,8 @@ class Settings(BaseSettings):
     BASE_SENDER_EMAIL_ADDRESS: str
     SLACK_BOT_USER_TOKEN: str
     SLACK_BOT_DEFAULT_NAME: str
+
+    model_config = SettingsConfigDict(env_file=(".env"))
 
 
 @lru_cache
